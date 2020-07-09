95fe235b14671d9e55419bf50e42fb45c4983696
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-07-09 04:06:58 +0300

    Allowed CORS from front-end

--- src/main/java/com/brandon/restaurant_reservation_system/restaurants/controller/RestaurantController.java
+++ src/main/java/com/brandon/restaurant_reservation_system/restaurants/controller/RestaurantController.java
@@ -2,13 +2,17 @@ package com.brandon.restaurant_reservation_system.restaurants.controller;
 
 import com.brandon.restaurant_reservation_system.GlobalVariables;
 import com.brandon.restaurant_reservation_system.helpers.date_time.services.DateTimeHandler;
-import com.brandon.restaurant_reservation_system.restaurants.data.RestaurantStub;
 import com.brandon.restaurant_reservation_system.restaurants.model.DateRange;
 import com.brandon.restaurant_reservation_system.restaurants.model.Restaurant;
 import com.brandon.restaurant_reservation_system.restaurants.services.TableAllocatorService;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -17,25 +21,25 @@ import java.util.Set;
 
 import static com.brandon.restaurant_reservation_system.helpers.date_time.services.DateTimeHandler.parseDate;
 
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
 public class RestaurantController {
-
-	private final Restaurant restaurant;
-	private final TableAllocatorService tableAllocator;
+	@Autowired
+	private Restaurant restaurant;
+	@Autowired
+	private TableAllocatorService tableAllocator;
 	DateTimeFormatter dateFormat =
 			GlobalVariables.getDateFormat();
 	private DateTimeHandler dateTimeHandler;
 
 	public RestaurantController() {
-		RestaurantStub restaurantStub = new RestaurantStub();
-		restaurant = restaurantStub.getRestaurant();
-		tableAllocator = new TableAllocatorService(restaurant);
-
 	}
 	//TEMPORARY STUBS
 
 	//GET /restaurant/dates
+	@CrossOrigin
 	@GetMapping("/bookings/free-dates")
-	public JSONObject getDate() {
+	public ResponseEntity<String> getDate() {
 		JSONObject json = new JSONObject();
 		DateRange range =
 				restaurant.getBookingDateRange();
@@ -43,7 +47,13 @@ public class RestaurantController {
 		json.put("end", range.getEnd());
 		json.put("availableDates", tableAllocator.getAvailableDates());
 
-		return json;
+		System.out.println(json);
+
+		ResponseEntity<String> response = new ResponseEntity<>(json.toString(),
+				HttpStatus.OK);
+
+		System.out.println(response);
+		return response;
 	}
 
 	@GetMapping("/bookings/date/{date}/size/{size}")
