2629898f0ff9821880f79492632c5ced24dee6af
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-07-14 11:29:48 +0300

    Added exception if restaurant is not configured correctly

---
+++ src/main/java/com/brandon/restaurant_reservation_system/restaurants/exceptions/RestaurantConfigurationException.java
@@ -0,0 +1,10 @@
+package com.brandon.restaurant_reservation_system.restaurants.exceptions;
+
+public class RestaurantConfigurationException extends RuntimeException {
+
+    private static final long serialVersionUID = -4997584171121456402L;
+
+    public RestaurantConfigurationException(String message) {
+        super("Restaurant " + message + " have not been configured correctly.");
+    }
+}
