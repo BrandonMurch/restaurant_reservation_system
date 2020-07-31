2f5d10ac232e728cd0216a3e4f5a77c1f7da76df
BrandonMurch <36788304+brandonmurch@users.noreply.github.com>
2020-07-31 12:54:02 +0300

    Add JwtUserDetailsService test

---
+++ src/test/java/com/brandon/restaurant_reservation_system/security/service/JwtUserDetailsServiceTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Brandon Murch
+ */
+
+package com.brandon.restaurant_reservation_system.security.service;
+
+import com.brandon.restaurant_reservation_system.users.CreateUsersForTesting;
+import com.brandon.restaurant_reservation_system.users.data.LoginableRepository;
+import com.brandon.restaurant_reservation_system.users.exceptions.UserNotFoundException;
+import com.brandon.restaurant_reservation_system.users.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+class JwtUserDetailsServiceTest {
+
+    @Mock
+    private LoginableRepository loginableRepository;
+    @InjectMocks
+    private JwtUserDetailsService userDetailsService;
+
+    @Test
+    void loadUserByUsername() {
+        User user = CreateUsersForTesting.createUser1();
+        Mockito.when(loginableRepository.findByUsername(any(String.class)))
+          .thenReturn(Optional.of(user));
+        var userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+        assertEquals(user.getUsername(), userDetails.getUsername());
+        assertEquals(user.getPassword(), userDetails.getPassword());
+    }
+
+    @Test
+    void loadUserByUsernameNotFound() {
+        Mockito.when(loginableRepository.findByUsername(any(String.class)))
+          .thenReturn(Optional.empty());
+        Exception exception = assertThrows(UserNotFoundException.class, () ->
+          userDetailsService.loadUserByUsername("username"));
+
+        assertTrue(exception.getMessage().contains("User was not found"));
+    }
+}
