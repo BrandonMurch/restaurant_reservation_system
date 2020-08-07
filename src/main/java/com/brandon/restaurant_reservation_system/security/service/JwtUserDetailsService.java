a625172dfef82d86d56bf60a4a145299d2689410
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-08-07 14:44:45 +0300

    Fix lazy loading exception by adding transactional
    getPermissions was throwing an exception due to
    the lazy loading. Adding Transactional annotation
    fixed this.

--- src/main/java/com/brandon/restaurant_reservation_system/security/service/JwtUserDetailsService.java
+++ src/main/java/com/brandon/restaurant_reservation_system/security/service/JwtUserDetailsService.java
@@ -16,9 +16,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class JwtUserDetailsService implements UserDetailsService {
 
     @Autowired
@@ -35,13 +37,13 @@ public class JwtUserDetailsService implements UserDetailsService {
             throw new UserNotFoundException("User was not found");
         } else {
             Loginable user = result.get();
-            // TODO: Permissions.
             return new org.springframework.security.core.userdetails.User(
               user.getUsername(), user.getPassword(), user.getPermissions()
             );
         }
     }
 
+    // todo: move this to user handler?
     public User saveUser(UserRegisterRequest newUser) {
         User user = new User(
           newUser.getFirstName(),
