0b302ef7bae36e0d9701d51cb4f3bbdb3d6cf0c4
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-08-11 09:19:52 +0300

    Refactor so cache updates are after response.
    Modify uriBuilder to remove /{id} if already present.
    
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- src/main/java/com/brandon/restaurant_reservation_system/bookings/controller/BookingController.java
+++ src/main/java/com/brandon/restaurant_reservation_system/bookings/controller/BookingController.java
@@ -22,6 +22,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -75,6 +79,7 @@ public class BookingController {
 		}
 	}
 
+	//	@GetMapping("/dailyCount"
 	@GetMapping("/{bookingId}")
 	public Booking getBookingById(@PathVariable long bookingId) {
 		return bookingRepository.findById(bookingId)
@@ -82,9 +87,11 @@ public class BookingController {
 	}
 
 	@PutMapping("/{bookingId}")
-	public ResponseEntity<?> updateBooking(
+	public void updateBooking(
 	@RequestBody Booking newBooking,
-	@PathVariable long bookingId
+	@PathVariable long bookingId,
+	HttpServletRequest request,
+	HttpServletResponse response
 	) {
 
 		Optional<Booking> result =
@@ -93,46 +100,73 @@ public class BookingController {
 		if (result.isPresent()) {
 			Booking booking = result.get();
 			booking.updateBooking(newBooking);
-			return new ResponseEntity<>("Booking successfully updated.",
-			HttpStatus.NO_CONTENT);
+			try {
+				sendResponse(response, HttpStatus.NO_CONTENT.value(), "Booking successfully " +
+				"updated.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			// TODO: update cache for date availability and number of bookings per day
 		} else {
-			ResponseEntity<?> response =
 			this.createBooking(
 			new RequestBodyUserBooking(newBooking.getUser(),
-			newBooking));
-			if (response.getStatusCode() == HttpStatus.CREATED) {
-				return ResponseEntity.created(
-				ServletUriComponentsBuilder
-				.fromCurrentRequest().build().toUri())
-				.build();
-			}
-			return response;
+			newBooking), response);
 		}
 	}
 
+	public void sendResponse(HttpServletResponse response, ResponseEntity<?> entity) throws IOException {
+		PrintWriter writer = response.getWriter();
+		response.setStatus(entity.getStatusCodeValue());
+		writer.print(entity.getBody());
+		writer.flush();
+		writer.close();
+	}
+
+	public <T> void sendResponse(HttpServletResponse response, int status,
+								 T body) throws IOException {
+		PrintWriter writer = response.getWriter();
+		response.setStatus(status);
+		writer.print(body);
+		writer.flush();
+		writer.close();
+	}
+
 	@PostMapping("")
-	public ResponseEntity<?> createBooking(
-	@RequestBody RequestBodyUserBooking body) {
+	public void createBooking(
+	@RequestBody RequestBodyUserBooking body,
+	HttpServletResponse response) {
 
 		Booking booking = body.getBooking();
 		Optional<ResponseEntity<ApiError>> bookingValidationException =
 		BookingValidationService.validateBooking(booking);
 		if (bookingValidationException.isPresent()) {
-			return bookingValidationException.get();
+			try {
+				sendResponse(response, bookingValidationException.get());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return;
 		}
 
 		User user = new User(body.getUser());
-		if (user.getEmail() == null) {
+		if (user.getUsername() == null) {
 			throw new BookingRequestFormatException("Email is a required field");
 		}
 		Booking result = bookingHandler.createBooking(booking, user);
+		try {
+			sendResponse(response, 201, buildUriFromBooking(result));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		restaurant.removeDateIfUnavailable(booking.getStartTime().toLocalDate());
-		return buildUriFromBooking(result);
+		// TODO: add booking to date in cache
 	}
 
 	private ResponseEntity<String> buildUriFromBooking(Booking booking) {
 		URI location = ServletUriComponentsBuilder
 		.fromCurrentRequest()
+		.replacePath("/bookings")
 		.path("/{id}")
 		.buildAndExpand(booking.getId())
 		.toUri();
